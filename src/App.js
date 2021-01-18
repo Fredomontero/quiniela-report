74838f4aabcbce1a6327e4f58d60c78891cac7ea
Fredomontero <christianmontero08@gmail.com>
2021-01-17 22:18:50 -0800

    Add assets and parse input data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  let input = React.createRef();
+
+  const parseData = () => {
+    let inputData = input.current.value;
+    let matches = inputData.toLowerCase().split(/[\n]/);
+    console.log(matches);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiniela Report</h1>
+      <div>
+        <textarea 
+          ref={input}
+          rows={15}
+          cols={40}
+        />
+      </div>
+      <button className="analize" onClick={parseData}>
+        Analyze
+      </button>
     </div>
   );
 }
---
+++ src/utils/teams.js
@@ -0,0 +1,93 @@
+const teams = [
+  {
+    name: "America",
+    code: "AME",
+    img: ""
+  },
+  {
+    name: "Monterrey",
+    code: "MON",
+    img: ""
+  },
+  {
+    name: "Pumas",
+    code: "PUM",
+    img: ""
+  },
+  {
+    name: "Santos",
+    code: "SANT",
+    img: ""
+  },
+  {
+    name: "Toluca",
+    code: "TOL",
+    img: ""
+  },
+  {
+    name: "Puebla",
+    code: "PUE",
+    img: ""
+  },
+  {
+    name: "Necaxa",
+    code: "NEC",
+    img: ""
+  },
+  {
+    name: "TIGRES",
+    code: "TIG",
+    img: ""
+  },
+  {
+    name: "Queretaro",
+    code: "QUE",
+    img: ""
+  },
+  {
+    name: "Mazatlan",
+    code: "MAZ",
+    img: ""
+  },
+  {
+    name: "Guadalajara",
+    code: "GUA",
+    img: ""
+  },
+  {
+    name: "Juarez",
+    code: "JUA",
+    img: ""
+  },
+  {
+    name: "Tijuana",
+    code: "TIJ",
+    img: ""
+  },
+  {
+    name: "Pachuca",
+    code: "PAC",
+    img: ""
+  },
+  {
+    name: "Atl. San Luis",
+    code: "LUIS",
+    img: ""
+  },
+  {
+    name: "Cruz Azul",
+    code: "AZUL",
+    img: ""
+  },
+  {
+    name: "Leon",
+    code: "LEON",
+    img: ""
+  },
+  {
+    name: "Atlas",
+    code: "ATLAS",
+    img: ""
+  },
+
+]
